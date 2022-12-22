d28335a4fabf7184d0eb367c4503a77c704d6714
steph <stephanie.k.springer@gmail.com>
2022-12-22 12:17:55 +0000

    Pending changes exported from your codespace

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -17,6 +17,6 @@ with open("laureates.csv", "r") as f:
     laureates = list(reader)
 
 for laureate in laureates:
-    if laureate["surname"] == "Einstein":
+    if laureate["name"] == "Marie":
         pprint(laureate)
         break
